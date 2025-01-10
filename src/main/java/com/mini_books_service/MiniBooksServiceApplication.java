5be616c350601e57848ffaf2bc3bb6d6c3e89cba
sequint <steve.e.quint@gmail.com>
2025-01-10 10:19:52 -0800

    first commit

---
+++ src/main/java/com/mini_books_service/MiniBooksServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mini_books_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBooksServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBooksServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mini_books_service/MiniBooksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini_books_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBooksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
