994496dd25cdb90a5eb5b4e1a057c003f2299cca
sequint <steve.e.quint@gmail.com>
2025-01-13 11:04:34 -0800

    Create a user model

---
+++ src/main/java/com/mini_books_service/models/User.java
@@ -0,0 +1,35 @@
+package com.mini_books_service.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private Integer id;
+    private String name;
+    private String email;
+    private String passwordHash;
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPasswordHash(String password) {
+        this.passwordHash = password;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return passwordHash;
+    }
+}
