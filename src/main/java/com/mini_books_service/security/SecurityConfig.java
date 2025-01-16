254e177c679776565d15085d78c612581f78e1e1
sequint <steve.e.quint@gmail.com>
2025-01-16 13:17:59 -0800

    Security Updates: - Add two beans:   - Security filter chain for dev   - Password encoder for user pass

--- src/main/java/com/mini_books_service/security/SecurityConfig.java
+++ src/main/java/com/mini_books_service/security/SecurityConfig.java
@@ -2,12 +2,25 @@ package com.mini_books_service.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // UPDATE SECURITY RULES AFTER DEV COMPLETE
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll()
+            )
+            .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
